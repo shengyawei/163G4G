b51fdabd9546b50473c9fdc776d5498d94aac34c
shengyawei <1850461586@qq.com>
2017-12-19 11:24:19 +0800

    1，盛亚伟提交第五章上级作业

---
+++ Hibernate/T5/29盛亚伟/上级作业/T5/src/com/qhit/lh/g4/syw/t5/service/impl/BaseServiceImpl.java
@@ -0,0 +1,29 @@
+package com.qhit.lh.g4.syw.t5.service.impl;
+
+import java.util.List;
+
+import javax.xml.registry.infomodel.User;
+
+import com.qhit.lh.g4.syw.t5.dao.BaseDao;
+import com.qhit.lh.g4.syw.t5.dao.impl.BaseDaoImpl;
+import com.qhit.lh.g4.syw.t5.service.BaseService;
+
+public class BaseServiceImpl implements BaseService {
+	private BaseDao baseDao = new BaseDaoImpl();
+	public void add(Object obj) {
+		baseDao.add(obj);
+	}
+
+	public void delete(Object obj) {
+		baseDao.delete((User) obj);
+	}
+
+	public void update(Object obj) {
+		baseDao.update((User) obj);
+	}
+
+	public List<Object> queryAll(String tableName) {
+		return baseDao.queryAll(tableName);
+	}
+
+}
